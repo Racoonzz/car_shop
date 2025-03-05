cb9c99a092e56171f0b48016445cd33211790b2e
kolozsvari <106528317+kolozsvari@users.noreply.github.com>
2025-03-05 14:04:07 +0100

    products  test

--- app/Http/Controllers/ModelController.php
+++ app/Http/Controllers/ModelController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Model; // assuming you have a Model model for this table
+use Illuminate\Database\Eloquent\Model as EloquentModel;
 
 class ModelController extends Controller
 {
     public function index()
     {
-        $models = Model::all(); // Fetch all models from the database
+        $models = EloquentModel::all(); // Fetch all models from the database
         return response()->json($models);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,18 @@ use App\Http\Controllers\OrderDetailController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarModelController;
-
+use App\Http\Controllers\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::post('/orders', [OrderController::class, 'store']);
 Route::post('/orderdetails', [OrderDetailController::class, 'store']);
-Route::delete('/orderdetails', [OrderDetailController::class, 'destroy']);
 Route::get('/models', [CarModelController::class, 'index']);
+Route::delete('/ordersDetails/{id}', [OrderDetailController::class, 'destroy'])->name('ordersDetails.destroy');
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
